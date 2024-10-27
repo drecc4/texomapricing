cb289dd269f5f38ca36af8d16625c92f59da821e
drecc4 <80606130+drecc4@users.noreply.github.com>
2024-10-26 21:47:43 -0500

    Update app.py

--- app.py
+++ app.py
@@ -156,7 +156,7 @@ col3.write(f"{price_denture_low}")
 col4.write(f"{price_denture_high}")
 
 col1.write("Dentures New Patient Premium")
-col2.write(f"###### -")
+col2.write(f"###### 0")
 col3.write(f"{price_denture_premium}")
 col4.write(f"{price_denture_premium}")
 
@@ -166,7 +166,7 @@ col3.write(f"{price_implant}")
 col4.write(f"{price_implant}")
 
 col1.write("###### Total Gross")
-col2.write(f"###### -")
+col2.write(f"###### 0")
 col3.write(f"###### {total_gross_low}")
 col4.write(f"###### {total_gross_high}")
 
